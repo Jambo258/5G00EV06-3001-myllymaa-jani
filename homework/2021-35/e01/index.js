fce342bffc88350f301e03ffe8ae697994e26caa
jambo258 <jambomyllymaa@gmail.com>
2021-09-05 16:48:49 +0300

    e01

---
+++ homework/2021-35/e01/index.js
@@ -0,0 +1,13 @@
+//if(false){
+//    console.log("Hello");
+//}
+/*
+let x = 4
+if(x < 10){
+    console.log("hello");
+}
+*/
+
+var generate = require("project-name-generator");
+
+console.log(generate().dashed);
